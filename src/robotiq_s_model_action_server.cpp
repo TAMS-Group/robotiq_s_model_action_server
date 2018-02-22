9147f942e8d0c2bc71a15c91fc4f3d9ab7eabbd0
Benjamin Scholz <2scholz@informatik.uni-hamburg.de>
2018-02-22 20:27:43 +0100

    Rewrote big parts of the action server
    While previously a mode had to be set at launch, now the mode is set
    depending on the chosen target.
    The server was changed to a FollowJointTrajectory action server.
    The last point of the trajectory is compared to hard coded joint states
    that correspond the target joint states of the different grasp types.
    Each target joint state has a corresponding hard coded GripperOutput
    that is then published.

--- src/robotiq_s_model_action_server.cpp
+++ src/robotiq_s_model_action_server.cpp
@@ -5,129 +5,101 @@
 
 #include "robotiq_action_server/robotiq_s_model_action_server.h"
 
-// To keep the fully qualified names managable
-
-//Anonymous namespaces are file local -> sort of like global static objects
-namespace
-{
-  using namespace robotiq_action_server;
-
-  /*  This struct is declared for the sole purpose of being used as an exception internally
-      to keep the code clean (i.e. no output params). It is caught by the action_server and 
-      should not propogate outwards. If you use these functions yourself, beware.
-  */
-  struct BadArgumentsError {};
-
-
-  GripperOutput goalToRegisterState(const GripperCommandGoal& goal, const SModelGripperParams& params)
-  {
-    GripperOutput result;
-    result.rACT = 0x1; // active gripper
-    result.rGTO = 0x1; // go to position
-    result.rATR = 0x0; // No emergency release
-    if (params.gripper_mode_ == "wide_pinch")
-    {
-      result.rICS = 1;   // individual control of scissor
-      result.rSPA = 80;  // finger speed; 80 for this specific case
-      result.rSPS = 255; // scissor speed
-    }
-    else // basic mode
-    {
-      result.rSPA = 128;  // finger speed; 80 for this specific case
-    }
-
-    if (goal.command.position > params.max_rad_ || goal.command.position < params.min_rad_)
-    {
-      ROS_WARN("Goal gripper rad size is out of range(%f to %f): %f m",
-               params.min_rad_, params.max_rad_, goal.command.position);
-      throw BadArgumentsError();
-    }
-
-    if (goal.command.max_effort < params.min_effort_ || goal.command.max_effort > params.max_effort_)
-    {
-      ROS_WARN("Goal gripper effort out of range (%f to %f N): %f N",
-               params.min_effort_, params.max_effort_, goal.command.max_effort);
-      throw BadArgumentsError();
-    }
-
-    // The register value for the closed pinch mode is 113,
-    // the minimal register value for the finger is 6 , so wie divide with 107
-    // analog for the basic mode
-
-    double dist_per_tick;
-    if (params.gripper_mode_ == "wide_pinch")
-      dist_per_tick = (params.max_rad_ - params.min_rad_) / 107;
-    else // basic mode
-      dist_per_tick = (params.max_rad_ - params.min_rad_) / 235;
-
-    double eff_per_tick = (params.max_effort_ - params.min_effort_) / 255;
-
-    result.rPRA = static_cast<uint8_t>((goal.command.position) / dist_per_tick);
-    result.rFRA = static_cast<uint8_t>((goal.command.max_effort - params.min_effort_) / eff_per_tick);
-
-    /* Register Value for scissor position: calculatet with the position of fingers (between 6 and 113)
-       multiplied with max scissor position divided by max finger position.
-       I'm not sure why it musst be 212 and 113 and noch 197 and 107, cause the scissor position starts with 15 and 
-       the finger position with 6.
-    */
-
-    if (params.gripper_mode_ == "wide_pinch")
-      result.rPRS = static_cast<uint8_t>(result.rPRA*(212.0/113.0));
-
-    ROS_INFO("Setting goal position register to %hhu", result.rPRA);
-
-    return result;
-  }
-
-  /*  This function is templatized because both GripperCommandResult and GripperCommandFeedback consist
-      of the same fields yet have different types. Templates here act as a "duck typing" mechanism to avoid
-      code duplication.
-  */
-  template<typename T>
-  T registerStateToResultT(const GripperInput& input, const SModelGripperParams& params, uint8_t goal_pos)
-  {
-    T result;
-
-    double dist_per_tick;
-    if (params.gripper_mode_ == "wide_pinch")
-      dist_per_tick = (params.max_rad_ - params.min_rad_) / 107;
-    else // basic mode
-      dist_per_tick = (params.max_rad_ - params.min_rad_) / 255;
-
-    double eff_per_tick = (params.max_effort_ - params.min_effort_) / 255;
-
-    result.position = input.gPOA * dist_per_tick + params.min_rad_;
-    result.effort = input.gCUA * eff_per_tick + params.min_effort_;
-    result.stalled = input.gIMC == 0x1 || input.gIMC == 0x2;
-    result.reached_goal = input.gPOA == goal_pos;
-
-    return result;
-  }
-
-  // Inline api-transformers to avoid confusion when reading the action_server source
-  inline
-  GripperCommandResult registerStateToResult(const GripperInput& input, const SModelGripperParams& params, uint8_t goal_pos)
-  {
-    return registerStateToResultT<GripperCommandResult>(input, params, goal_pos);
-  }
-
-  inline
-  GripperCommandFeedback registerStateToFeedback(const GripperInput& input, const SModelGripperParams& params, uint8_t goal_pos)
-  {
-    return registerStateToResultT<GripperCommandFeedback>(input, params, goal_pos);
-  }
-
-} // end of anon namespace
-
 namespace robotiq_action_server
 {
 
-SModelGripperActionServer::SModelGripperActionServer(const std::string& name, const SModelGripperParams& params)
-  : nh_()
-  , as_(nh_, name, false)
-  , action_name_(name)
-  , gripper_params_(params)
+SModelGripperActionServer::SModelGripperActionServer(const std::string& name)
+  : nh_(),
+    as_(nh_, name, false),
+    action_name_(name)
 {
+  ros::NodeHandle pn("~");
+
+  std::vector<double> basic_open_joint_states = {0.107314, 0.0, -0.113446, 0.107314, 0.0, -0.113446, 
+                                                 0.107314, 0.0, -0.113446, -0.016212, 0.016212};
+  std::vector<double> basic_closed_joint_states = {1.22173, 1.5708, -0.959931, 1.22173, 1.5708, -0.959931, 
+                                                   1.22173, 1.5708, -0.959931, -0.016212, 0.016212};
+  std::vector<double> pinch_open_joint_states = {0.0495296, 0.0, -0.0523599, 0.0495296, 0.0, -0.0523599,
+                                                 0.0495296, 0.0, -0.0523599, -0.1561488, 0.1561488};
+  std::vector<double> pinch_closed_joint_states = {0.9328077, 0.0, -0.9861110, 0.9328077, 0.0, -0.9861110,
+                                                   0.9328077, 0.0, -0.9861110, -0.1561488, 0.1561488};
+  std::vector<double> wide_open_joint_states = {0.0495296, 0.0, -0.0523599, 0.0495296, 0.0, -0.0523599,
+                                                0.0495296, 0.0, -0.0523599, 0.1766273, -0.1766273};
+  std::vector<double> wide_closed_joint_states = {1.2217304, 1.5707963, -0.9599310, 1.2217304, 1.5707963, -0.9599310,
+                                                  1.2217304, 1.5707963, -0.9599310, 0.1766273, -0.1766273};
+  std::vector<double> scissor_closed_joint_states = {0.0495296, 0.0, -0.0523599, 0.0495296, 0.0, -0.0523599,
+                                                     0.0495296, 0.0, -0.0523599, -0.1783338, 0.1783338};
+  std::vector<double> wide_pinch_open_joint_states = {0.0495296, 0.0, -0.0523599, 0.0495296, 0.0, -0.0523599, 
+                                                      0.0495296, 0.0, -0.0523599, 0.191986, -0.191986};
+  std::vector<double> wide_pinch_closed_joint_states = {0.924553, 0.0, -0.977384, 0.924553, 0.0, -0.977384,
+                                                        0.924553, 0.0, -0.977384, -0.14079, 0.14079};
+
+  pn.param("gripper_start_mode", gripper_start_mode_, std::string("basic"));
+  pn.param("gripper_speed", gripper_speed_, 128);
+
+  if (gripper_speed_ < 0 || gripper_speed_ > 255)
+  {
+    ROS_WARN("Gripper speed was not set between 0 and 255. Defaulting to 128");
+    gripper_speed_ = 128;
+  }
+
+  // Setting default values that are shared for all grasps
+  GripperOutput go;
+  go.rACT = 1;
+  go.rGTO = 1;
+  go.rSPA = gripper_speed_;
+  go.rFRA = 150;
+
+  // basic grasps
+  go.rMOD = 0;
+  go.rPRA = 0;
+  if (!basic_open_joint_states.empty())
+    joint_state_to_reg_state_[basic_open_joint_states] = go;
+  go.rPRA = 255;
+  if (!basic_closed_joint_states.empty())
+    joint_state_to_reg_state_[basic_closed_joint_states] = go;
+
+  // pinch grasps
+  go.rMOD = 1;
+  go.rPRA = 0;
+  if (!pinch_open_joint_states.empty())
+    joint_state_to_reg_state_[pinch_open_joint_states] = go;
+  go.rPRA = 255;
+  if (!pinch_closed_joint_states.empty())
+    joint_state_to_reg_state_[pinch_closed_joint_states] = go;
+
+  // wide grasps
+  go.rMOD = 2;
+  go.rPRA = 0;
+  if (!wide_open_joint_states.empty())
+    joint_state_to_reg_state_[wide_open_joint_states] = go;
+  go.rPRA = 255;
+  if (!wide_closed_joint_states.empty())
+    joint_state_to_reg_state_[wide_closed_joint_states] = go;
+
+  // scissor grasps
+  // wide pinch open is identical to scissor open, so it is used instead
+  go.rMOD = 3;
+  go.rPRA = 0;
+  go.rPRA = 255;
+  if (!scissor_closed_joint_states.empty())
+    joint_state_to_reg_state_[scissor_closed_joint_states] = go;
+
+  // wide pinch grasps
+  go.rMOD = 0;
+  go.rICS = 1;
+  go.rPRA = 6;
+  go.rSPA = 80; // Hardcoded for now. Different values could lead to unexpected behavior
+  go.rFRA = 127; // See comment above
+  go.rPRS = 11;
+  go.rSPS = 255;
+  if (!wide_pinch_open_joint_states.empty())
+    joint_state_to_reg_state_[wide_pinch_open_joint_states] = go;
+  go.rPRA = 111;
+  go.rPRS = 208;
+  if (!wide_pinch_closed_joint_states.empty())
+    joint_state_to_reg_state_[wide_pinch_closed_joint_states] = go;
+
   as_.registerGoalCallback(boost::bind(&SModelGripperActionServer::goalCB, this));
   as_.registerPreemptCallback(boost::bind(&SModelGripperActionServer::preemptCB, this));
 
@@ -145,25 +117,25 @@ void SModelGripperActionServer::goalCB()
     return;
   }
 
-  GripperCommandGoal current_goal (*(as_.acceptNewGoal()));
+  FollowJointTrajectoryGoal current_goal (*(as_.acceptNewGoal()));
 
   if (as_.isPreemptRequested())
   {
     as_.setPreempted();
   }
 
-  try
+  std::vector<double> current_goal_state = current_goal.trajectory.points.back().positions;
+
+  // find equal joint state
+  auto it = find_if(joint_state_to_reg_state_.begin(), joint_state_to_reg_state_.end(), [this, current_goal_state] (const std::pair<std::vector<double>, GripperOutput>& s) { return compare_joint_states(s.first, current_goal_state, 0.001); } );
+  if(it != joint_state_to_reg_state_.end())
   {
-    if (current_goal.command.max_effort == 0.0){
-      ROS_INFO("requested to move gripper with max_effort of 0.0. Defaulting to 70.0");
-      current_goal.command.max_effort = 70.0;
-    }
-    goal_reg_state_ = goalToRegisterState(current_goal, gripper_params_);
-    goal_pub_.publish(goal_reg_state_);
+     goal_reg_state_ = it->second;
+     goal_pub_.publish(goal_reg_state_);
   }
-  catch (BadArgumentsError& e)
+  else
   {
-    ROS_INFO("%s No goal issued to gripper", action_name_.c_str());
+    ROS_WARN("Provided grasp is not known.");
   }
 }
 
@@ -196,26 +168,20 @@ void SModelGripperActionServer::analysisCB(const GripperInput::ConstPtr& msg)
   if (current_reg_state_.gFLT)
   {
     ROS_WARN("%s faulted with code: %x", action_name_.c_str(), current_reg_state_.gFLT);
-    as_.setAborted(registerStateToResult(current_reg_state_,
-                                         gripper_params_,
-                                         goal_reg_state_.rPRA));
+    FollowJointTrajectoryResult action_res;
+    action_res.error_code = -5;
+    as_.setAborted(action_res);
   }
   else if (current_reg_state_.gGTO && current_reg_state_.gIMC == 0x03 && current_reg_state_.gPRA == goal_reg_state_.rPRA && current_reg_state_.gSTA)
   {
     // If commanded to move and if at a goal state and if the position request matches the echo'd PR, we're
     // done with a moveregisterStateToR
     ROS_INFO("%s succeeded", action_name_.c_str());
-    as_.setSucceeded(registerStateToResult(current_reg_state_,
-                                           gripper_params_,
-                                           goal_reg_state_.rPRA));
+    FollowJointTrajectoryResult action_res;
+    action_res.error_code = 0;
+    as_.setSucceeded(action_res);
   }
-  else
-  {
-    // Publish feedback
-    as_.publishFeedback(registerStateToFeedback(current_reg_state_,
-                                                gripper_params_,
-                                                goal_reg_state_.rPRA));
-  }
+  // We could publish feedback here using the current_reg_state_.
 }
 
 void SModelGripperActionServer::issueActivation()
@@ -223,15 +189,47 @@ void SModelGripperActionServer::issueActivation()
   ROS_INFO("Activating gripper for gripper action server: %s", action_name_.c_str());
   GripperOutput out;
   out.rACT = 0x1;
-  out.rSPA = 128;
+  out.rSPA = gripper_speed_;
 
-  if (gripper_params_.gripper_mode_ == "wide_pinch")
+  if (gripper_start_mode_ == "pinch")
+    out.rMOD = 0x1;
+  else if (gripper_start_mode_ == "wide")
+    out.rMOD = 0x2;
+  else if (gripper_start_mode_ == "scissor")
     out.rMOD = 0x3;
   else
+  {
+    if (gripper_start_mode_ != "basic")
+    {
+      ROS_WARN("No valid gripper start mode was given. Setting to basic mode.");
+    }
     out.rMOD = 0x0;
+  }
 
   // other params should be zero
   goal_reg_state_ = out;
   goal_pub_.publish(out);
+}
+
+bool SModelGripperActionServer::compare_joint_states(const std::vector<double> &js1, const std::vector<double> &js2, double eps)
+{
+  if(js1.size() != js2.size())
+  {
+    ROS_WARN("The number of joints received deviates from the number of joints in the robotiq_s_model_action_server.");
+    return false;
+  }
+
+  // Using euclidean distance for comparison
+  double distance = 0.0;
+  for(int i=0; i < js1.size(); i++)
+  {
+    distance += std::pow(js1[i] - js2[i], 2.0);
+  }
+  distance = std::sqrt(distance);
+
+  if (distance <= eps)
+    return true;
+
+  return false;
 }
 } // end robotiq_action_server namespace
